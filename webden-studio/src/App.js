da8dd3c9f85e2ae05e5a98141e49a7b90872798c
MrKrasnov <mr.krasnov.y@gmail.com>
2020-02-10 22:09:50 +0200

    Connect component at app.js

--- webden-studio/src/App.js
+++ webden-studio/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.sass';
+// connect components
+import Header from "./components/header";
+import Main from "./components/main";
+import Dist from "./components/dist/dist";
+import Footer from "./components/dist/footer";
+
 
 function App() {
   return (
     <div>
-      
+      <Header />
+      <Main />
+      <Dist />
+      <Footer />
     </div>
   );
 }
