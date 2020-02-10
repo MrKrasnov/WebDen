be1b8c38a63d8f7550e446ec1ffbe1ae994e2a18
MrKrasnov <mr.krasnov.y@gmail.com>
2020-02-11 01:36:27 +0200

    Added title text at main.jsx

--- webden-studio/src/components/main.jsx
+++ webden-studio/src/components/main.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Main = () => {
     return(
         <section className="main">
-
+            <h1 className="main-title">
+                DEVELOPMENT AND PROMOTION <br/>
+                By WEBDEN.STUDIO
+            </h1>
         </section>
     );
 }
