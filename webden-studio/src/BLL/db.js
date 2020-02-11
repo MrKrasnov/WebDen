e107ac7c764e465c4b229094e44e3f289047c52a
MrKrasnov <mr.krasnov.y@gmail.com>
2020-02-11 02:44:55 +0200

    Filling at a state

--- webden-studio/src/BLL/db.js
+++ webden-studio/src/BLL/db.js
@@ -2,7 +2,40 @@ let state = {
     main: [
         {
             "title": "DEVELOPMENT AND PROMOTION",
-            "author": "WEBDEN.STUDIO"
+            "author": "WEBDEN.STUDIO",
+            "placeholder": "Создать сайт...",
+            "button": "ЗАКАЗАТЬ САЙТ",
+            "scroll": 'Листайте дальше'
+        }
+    ],
+    dist: [
+        {
+            "title": "Почему мы",
+            "distribution" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            list: [
+                {
+                    "id": "1",
+                    "text": "Адаптивный дизайн"
+                },
+                {
+                    "id": "2",
+                    "text": "Внутренняя SEO оптимизация"
+                },
+                {
+                    "id": "3",
+                    "text": "Инструменты маркетинга"
+                },
+                {
+                    "id": "4",
+                    "text": "Заботливая тех. поддержка"
+                },
+            ],
+            "feedback": "связаться с нами",
+            "email": "info@webden.studio",
+            "phone": "+38 063 863 97 49",
+            "skype": "webden.studio",
+            "defence": "© Webden.Studio 2020. Все права защищены",
+            "button": "отправить"
         }
     ]
 }
